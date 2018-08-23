0e1de692d138908a92a46dba083c82494ae049b0
Iskrova <mary.iskrova@gmail.com>
2018-08-23 12:23:27 +0300

    fix

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.demo.data")
 @EntityScan(basePackages = "com.example.demo.entities")
 public class DemoApplication {
 
